3643cac8c44d3bbbfe95e13e2583bddc75718925
wkershaw <14042628+wkershaw@users.noreply.github.com>
2023-08-31 18:52:52 +0100

    Delete day1/target directory

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/will/Documents/source/repos/advent_of_code/day1/target/debug/day1: /home/will/Documents/source/repos/advent_of_code/day1/src/input.txt /home/will/Documents/source/repos/advent_of_code/day1/src/main.rs
